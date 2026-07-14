87f866de71d84fbd16628def42c2ac281b413e0a
sebastian <nugrasebas2@gmail.com>
2026-07-13 23:58:52 -0500

    feat(controller): add operation lookup endpoint

--- src/main/java/dev/sebas/calculadora/controller/OperacionController.java
+++ src/main/java/dev/sebas/calculadora/controller/OperacionController.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,4 +40,9 @@ public class OperacionController {
         return ResponseEntity.ok(operacionService.obtenerHistorial(page, size));
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<HistorialResponse> operacion(@PathVariable Long id) {
+        HistorialResponse respuesta = operacionService.obtenerPorId(id);
+        return ResponseEntity.ok(respuesta);
+    }
 }
