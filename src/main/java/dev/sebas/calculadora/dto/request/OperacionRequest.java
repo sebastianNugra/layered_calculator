206d62c04dbf5463b0a032b1e01c72d4ae5275cd
sebastian <nugrasebas2@gmail.com>
2026-07-17 19:45:31 -0500

    refactor(dto): apply strict deserializer to valor1 and valor2 fields

--- src/main/java/dev/sebas/calculadora/dto/request/OperacionRequest.java
+++ src/main/java/dev/sebas/calculadora/dto/request/OperacionRequest.java
@@ -1,16 +1,20 @@
 package dev.sebas.calculadora.dto.request;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
 import dev.sebas.calculadora.model.TipoOperacion;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class OperacionRequest {
-    
+
     @NotNull
+    @JsonDeserialize(using = StrictDoubleDeserializer.class)
     private Double valor1;
 
     @NotNull
+    @JsonDeserialize(using = StrictDoubleDeserializer.class)
     private Double valor2;
 
     @NotNull
